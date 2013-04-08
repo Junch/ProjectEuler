2e558c44aa69e77f14bd8db04172dd7fe4c417ec
Junch <junc76@gmail.com>
2013-04-08 23:00:33 +0800

    The number is too big to overflow.
    I will use some libraries to do the work.

--- CppEuler/Euler.cpp
+++ CppEuler/Euler.cpp
@@ -1,9 +1,11 @@
 #include <algorithm>
 #include <iostream>
 
-int maxdiv(long m, long n)
+#define LLONG long long
+
+LLONG maxdiv(LLONG m, LLONG n)
 {
-    long a = m % n;
+    LLONG a = m % n;
     while (a != 0) {
         m = n;
         n = a;
@@ -13,22 +15,43 @@ int maxdiv(long m, long n)
     return n;
 }
 
-void normal(long& m, long& n)
+void normal(LLONG& m, LLONG& n)
 {
-    long div = maxdiv(m, n);
+    LLONG div = maxdiv(m, n);
     m /= div;
     n /= div;
 }
 
+int digits(LLONG n)
+{
+    int count = 0;
+    do {
+        count++;
+        n = n/10;
+    }while(n > 0);
+
+    return count;
+}
+
 void problem_57()
 {
-    using namespace std;
-    typedef pair<long, long> ND;
-    ND a(3,2);
-    long n = a.first + 2*a.second;
-    long d = a.first + a.second;
-    normal(n,d);
-    cout << n << " " << d << endl;
+    int count = 0;
+    LLONG n=1, d=1;
+    for (int i=0; i<1000; ++i)
+    {
+        LLONG t = n;
+        n = t + 2*d;
+        d = t + d;
+        normal(n,d);
+        int ndigit = digits(n);
+        int ddigit = digits(d);
+
+        if (ndigit > ddigit)
+            ++count;
+    }
+
+    std::cout << "n=" << n << " d=" << d << std::endl;
+    std::cout << "count=" << count << std::endl;
 }
 
 int main(int argc, char* argv)
